Fixed edge_jump and edge_mask calculation.

xanes_frameset.py
@@ -1436,9 +1436,10 @@ class XanesFrameset():
         """Plot the results of the edge jump filter."""
         if ax is None:
             ax = new_image_axes()
-        ej = self.edge_jump_filter()
-        artist = ax.imshow(ej, extent=self.extent(), cmap=self.cmap,
-                           origin="lower", alpha=alpha)
+        ej = self.edge_jump()
+        artist = ax.imshow(ej, extent=self.extent('absorbances'),
+                           cmap=self.cmap, origin="lower",
+                           alpha=alpha)
         ax.set_xlabel('µm')
         ax.set_ylabel('µm')
         return artist
@@ -1448,7 +1449,7 @@ class XanesFrameset():
         """Calculate a image showing the difference in
         signal across the X-ray edge."""
         with self.store() as store:
-            ej = xm.edge_jump(frames=store.absorbances,
+            ej = xm.edge_jump(frames=store.absorbances.value,
                               energies=store.energies.value,
                               edge=self.edge)
         return ej
@@ -1468,7 +1469,7 @@ class XanesFrameset():
           removed. Passing zero (default) will result in no effect.
         """
         with self.store() as store:
-            mask = xm.edge_mask(frames=store.absorbances,
+            mask = xm.edge_mask(frames=store.absorbances.value,
                                 energies=store.energies.value, edge=self.edge(),
                                 sensitivity=sensitivity, min_size=min_size)
         return mask
@@ -1543,7 +1544,7 @@ class XanesFrameset():
         masked_map = np.ma.array(map_data, mask=self.goodness_mask())
         return masked_map
 
-    def extent(self, representation, idx=0):
+    def extent(self, representation, idx=(0, 0)):
         """Determine physical dimensions for axes values.
 
         Returns: If idx was given, a single tuple of (left, right,
@@ -1560,7 +1561,7 @@ class XanesFrameset():
             frames = store.get_frames(representation)
             if idx is not None:
                 # Filter to only the requested frame
-                imshape = np.array(frames[idx].shape) # (rows, cols)
+                imshape = np.array(frames[idx].shape)[-2:] # (rows, cols)
                 pixel_size = store.pixel_sizes[idx]
                 center = store.relative_positions[idx]
             else:

xanes_math.py
@@ -218,7 +218,7 @@ def edge_mask(frames: np.ndarray, energies: np.ndarray, edge,
         min_size = (frames.shape[-1] + frames.shape[-2]) / 200
     # dividing the edge jump by the standard deviation provides sharp constrast
     ej = edge_jump(frames=frames, energies=energies, edge=edge)
-    stdev = np.std(frames, axis=0)
+    stdev = np.std(frames, axis=tuple(range(0, len(frames.shape)-2)))
     edge_ratio = ej / stdev
     # Thresholding  to separate background from foreground
     img_bottom = edge_ratio.min()
@@ -234,6 +234,9 @@ def edge_mask(frames: np.ndarray, energies: np.ndarray, edge,
 
 
 def edge_jump(frames: np.ndarray, energies: np.ndarray, edge):
+    COLS = -1
+    ROWS = -2
+    ENERGY = -3
     # Check that dimensions match
     if not frames.shape[0] == energies.shape[0]:
         msg = "First dimenions of frames and energies do not match ({} vs {})"