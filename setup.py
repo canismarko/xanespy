a26dd4c40ed0932cb19c3937fbcc17d142eea7c5
Mark Wolf <mark.wolf.music@gmail.com>
2016-12-26 16:03:50 -0500

    Created a setup.py and updated the travis.yml to install it.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="xanespy",
+      version="0.1",
+      description="Tools for analyzing X-ray absorbance spectroscopy data",
+      author="Mark Wolf",
+      author_email="mark.wolf.music@gmail.com",
+      url="https://github.com/m3wolf/xanespy",
+      packages=['xanespy',],
+)
